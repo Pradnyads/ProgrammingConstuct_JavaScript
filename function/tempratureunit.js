30616b082a46103dfe2d2f7ca0603adaa38bfaa5
Pradnyads <sorepradnya@gmail.com>
2020-12-03 15:51:16 +0530

    Programming Construct : Function Problems

---
+++ function/tempratureunit.js
@@ -0,0 +1,26 @@
+const cToF=1;
+const fToC=2;
+var readlineSync = require('readline-sync');
+let temperature=readlineSync.question("Enter temperature: ");
+let choice=readlineSync.question("Convert temperature into 1.Fahreheit 2.Celsius: ");
+
+    function getDegreeF(temperature){
+        console.log(temperature+" C = "+(temperature*9/5+32).toFixed(3)+" F");
+    }
+    function getDegreeC(temperature){
+        console.log(temperature+" F = "+((temperature-32)*5/9).toFixed(3)+" C");
+    }
+
+    switch(choice){
+        case 1   :   if(temperature >= 0 && temperature <=100)
+                            getDegreeF(temperature);
+                        else
+                            console.log("Temperatoure should be within 0-100 C.");
+                        break;
+        case 2   :   if(temperature >= 32 && temperature <=212)
+                            getDegreeC(temperature);
+                        else
+                            console.log("Temperatoure should be within 32-212 F.");
+                        break;
+        default :   console.log("invalid input.");
+    }
